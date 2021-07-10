a31f8df51f20df157d08796ddb0c17c0021288bc
meekle <meekle@mac.com>
2021-07-10 14:47:29 -0500

    basic routes updated with new dev branch

--- server/server.js
+++ server/server.js
@@ -58,15 +58,15 @@ app.post('/api/upload', (req, res) => {
 
 
 
-if (process.env.NODE_ENV === 'production') {
-  // statically serve everything in the build folder on the route '/build'
-  app.use('/build', express.static(path.join(__dirname, '../build')));
+//if (process.env.NODE_ENV === 'production') {
+// statically serve everything in the build folder on the route '/build'
+app.use('/build', express.static(path.join(__dirname, '../build')));
 
-  // serve index.html on the route '/'
-  app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-  });
-}
+// serve index.html on the route '/'
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+//}
 
 app.use ((req, res, next) => {
   res.status(404).send('Sorry can\'t find that!');
