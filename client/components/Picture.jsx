36367ed68fc132aa2f0859dd84596e90bcddd313
cjamesb <cjberry1234@sbcglobal.net>
2021-07-10 23:00:44 -0700

    commit before merge

--- client/components/Picture.jsx
+++ client/components/Picture.jsx
@@ -5,7 +5,7 @@ const Picture = props => (
     <div className='pictureHead'>
         <h1> Hello, picutre</h1>
         <img src={props.filepath}/>
-        <img src={'https://im.indiatimes.in/content/itimes/photo/2016/Aug/26/1472233327-25-pictures-that-will-instantly-make-you-happy_card.jpg'}/>
+        {/* <img src={'https://im.indiatimes.in/content/itimes/photo/2016/Aug/26/1472233327-25-pictures-that-will-instantly-make-you-happy_card.jpg'}/> */}
     </div>
 )
 
