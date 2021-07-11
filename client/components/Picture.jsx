6354aa0019ecff1221db329e703a9a9d3f9e371b
meekle <meekle@mac.com>
2021-07-10 22:50:03 -0500

    now serving images from server/images

--- client/components/Picture.jsx
+++ client/components/Picture.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 
 const Picture = props => (
     <div className='pictureHead'>
-        <h1> Hello, picutre</h1>
+        <h3> Wanted:</h3>
         <img src={props.filepath}/>
-        <img src={'https://im.indiatimes.in/content/itimes/photo/2016/Aug/26/1472233327-25-pictures-that-will-instantly-make-you-happy_card.jpg'}/>
     </div>
 )
 
--- client/reducers/photoReducer.js
+++ client/reducers/photoReducer.js
@@ -2,22 +2,23 @@ import * as types from '../constants/actionTypes';
 
 const initialState = {
   photoArray: [
-    {
-      filepath:
-        'https://im.indiatimes.in/content/itimes/photo/2016/Aug/26/1472233327-25-pictures-that-will-instantly-make-you-happy_card.jpg',
-    },
+    {filepath: './images/kingWobbegong.jpg'},
+    {filepath:'./images/image.jpg'},
+    {filepath: './images/wobbegong.jpg'},
+    {filepath: './images/wobbegong1.jpg'},
+    {filepath: './images/wobbegong2.jpg'}
   ],
 };
 
 const photoReducer = (state = initialState, action) => {
   switch (action.type) {
-    // case DO_A_THING:
-    //return {...state,
-    //changedKey:action.payload
-    //      secondChangedKey: ""};
-    //
-    default:
-      return state;
+  // case DO_A_THING:
+  //return {...state,
+  //changedKey:action.payload
+  //      secondChangedKey: ""};
+  //
+  default:
+    return state;
   }
 };
 
